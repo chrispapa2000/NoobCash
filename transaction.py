3ecb8aafc6d20a1a5d983f8611a94554d4a1e849
chrispapa2000 <el18017@mail.ntua.gr>
2023-03-11 10:47:44 +0200

    some progress with the transaction class

--- transaction.py
+++ transaction.py
@@ -26,14 +26,39 @@ class Transaction:
         #self.transaction_inputs: λίστα από Transaction Input 
         #self.transaction_outputs: λίστα από Transaction Output 
         #selfSignature
-        pass
 
+        self.sender_address = sender_address
+        self.receiver_address = recipient_address
+        self.amount = value
 
-    
+        # transaction as dict
+        d = {
+            "sender_address": self.sender_address,
+            "receiver_address": self.receiver_address,
+            "amount": self.amount
+        }
+        # transaction to hash
+        hash_object = SHA.new(data=binascii.a2b_qp(jsonify(d)))
+        # get the signature
+        signer = PKCS1_v1_5.new(sender_private_key)
+        #sign the hash object
+        signature = signer.sing(hash_object)
+
+        # assign the values 
+        self.transaction_id = hash_object
+        self.signature = signature
+
+        self.transaction_inputs = [{"previousOutputId":1}] # to be changed
+        self.transaction_outputs = [{"transaction_id":1, "receiver_address":1, "amount":1}] # to be changed    
 
 
     def to_dict(self):
-        pass
+        d = {
+            "sender_address": self.sender_address,
+            "receiver_address": self.receiver_address,
+            "amount": self.amount
+        }
+        return d
         
 
     def sign_transaction(self):
