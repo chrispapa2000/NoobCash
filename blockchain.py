96f074bf8f794fc110c571ed00303c9add57c87e
chrispapa2000 <el18017@mail.ntua.gr>
2023-03-11 16:47:25 +0200

    initialized blockchain class

---
+++ blockchain.py
@@ -0,0 +1,9 @@
+import block
+
+class Blockchain():
+    def __init__(self):
+        # initialize the list of blocks
+        list_of_blocks = []
+
+    def add_block(self, bl: block.Block):
+        self.list_of_block.append(bl)
